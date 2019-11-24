a90280c7e55fbecd1c7bf198a4561a750cf4ec55
Julian Benedikt Heuschen <julian@fheuschen.de>
2019-11-24 15:01:39 +0100

    Added product entity

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -30,6 +32,16 @@ class Image
      * @ORM\Column(type="string", length=255)
      */
     private $mime;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Product", mappedBy="thumbnail")
+     */
+    private $thumbnail_holders;
+
+    public function __construct()
+    {
+        $this->thumbnail_holders = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -68,6 +80,37 @@ class Image
     public function setMime(string $mime): self
     {
         $this->mime = $mime;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Product[]
+     */
+    public function getThumbnailHolders(): Collection
+    {
+        return $this->thumbnail_holders;
+    }
+
+    public function addThumbnailHolder(Product $thumbnailHolder): self
+    {
+        if (!$this->thumbnail_holders->contains($thumbnailHolder)) {
+            $this->thumbnail_holders[] = $thumbnailHolder;
+            $thumbnailHolder->setThumbnail($this);
+        }
+
+        return $this;
+    }
+
+    public function removeThumbnailHolder(Product $thumbnailHolder): self
+    {
+        if ($this->thumbnail_holders->contains($thumbnailHolder)) {
+            $this->thumbnail_holders->removeElement($thumbnailHolder);
+            // set the owning side to null (unless already changed)
+            if ($thumbnailHolder->getThumbnail() === $this) {
+                $thumbnailHolder->setThumbnail(null);
+            }
+        }
 
         return $this;
     }
---
+++ src/Entity/Product.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Image", inversedBy="thumbnail_holders")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $thumbnail;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $short_description;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="array")
+     */
+    private $images = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getThumbnail(): ?Image
+    {
+        return $this->thumbnail;
+    }
+
+    public function setThumbnail(?Image $thumbnail): self
+    {
+        $this->thumbnail = $thumbnail;
+
+        return $this;
+    }
+
+    public function getShortDescription(): ?string
+    {
+        return $this->short_description;
+    }
+
+    public function setShortDescription(?string $short_description): self
+    {
+        $this->short_description = $short_description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getImages(): ?array
+    {
+        return $this->images;
+    }
+
+    public function setImages(array $images): self
+    {
+        $this->images = $images;
+
+        return $this;
+    }
+}
